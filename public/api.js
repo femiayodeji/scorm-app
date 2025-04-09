dbe9543a61b11975f99d3438f25af009e6cc630a
femiayodeji <femidotayodeji@gmail.com>
2025-04-09 21:22:45 +0100

    Add course progress logic

--- public/api.js
+++ public/api.js
@@ -1,32 +1,30 @@
 console.log("SCORM API loaded!");
+const progressStore = {}; // You can replace this with a POST call to your server
 window.API = {
     LMSInitialize: function () {
-      console.log("LMSInitialize");
+      console.log("[SCORM] LMSInitialize");
       return "true";
     },
     LMSFinish: function () {
-      console.log("LMSFinish");
+      console.log("[SCORM] LMSFinish");
       return "true";
     },
     LMSGetValue: function (key) {
-      console.log("LMSGetValue:", key);
-      return "";
+      console.log(`[SCORM] LMSGetValue: ${key}`);
+      return progressStore[key] || "";
     },
     LMSSetValue: function (key, value) {
-      console.log("LMSSetValue:", key, value);
+      console.log(`[SCORM] LMSSetValue: ${key} = ${value}`);
+      progressStore[key] = value;
+      // Optional: send progress to server
+
       return "true";
     },
     LMSCommit: function () {
-      console.log("LMSCommit");
+      console.log("[SCORM] LMSCommit", progressStore);
       return "true";
     },
-    LMSGetLastError: function () {
-      return "0";
-    },
-    LMSGetErrorString: function () {
-      return "No error";
-    },
-    LMSGetDiagnostic: function () {
-      return "No diagnostics";
-    }
+    LMSGetLastError: () => "0",
+    LMSGetErrorString: () => "No error",
+    LMSGetDiagnostic: () => "No diagnostic"    
   };
